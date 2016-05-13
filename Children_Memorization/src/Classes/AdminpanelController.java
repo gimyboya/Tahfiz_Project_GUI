cb22a7d0c8b42b256845e298685c97afbfcf3fa2
gimy <b.afatah@yahoo.com>
2016-05-13 16:39:15 +0800

    clean the files
    deleted all the inececary files

--- Children_Memorization/src/Classes/AdminpanelController.java
+++
@@ -1,7 +0,0 @@
-package Classes;
-
-/**
- * Created by gimy on 11/05/2016.
- */
-public class AdminpanelController {
-}
--- Children_Memorization/src/Classes/TeacherpanelController.java
+++
@@ -1,9 +0,0 @@
-package Classes;
-
-import javafx.scene.control.ListView;
-/**
- * Created by gimy on 13/05/2016.
- */
-public class TeacherpanelController {
-
-}
